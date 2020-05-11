58782f019700347e989d60676b8a3fd167c8bb0f
pclank <31415965+pclank@users.noreply.github.com>
2020-05-11 18:27:36 +0300

    Initial Commit, including working App Settings from Nav Bar
    Signed-off-by: pclank <31415965+pclank@users.noreply.github.com>

--- app/src/main/java/com/example/chatify/MainActivity.kt
+++ app/src/main/java/com/example/chatify/MainActivity.kt
@@ -3,18 +3,17 @@ package com.example.chatify
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,10 +31,13 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+                R.id.appSettingsActivity, R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -47,12 +49,15 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-    /*
-    fun settingsButton()                    //Settings Button Press
-    {
-        val intent = Intent(this, AppSettingsActivity::class.java).apply{};
 
-        startActivity(intent);
+
+    fun settingsButton(view: View)                    //Settings Button Press
+    {
+        val intent = Intent(this, AppSettingsActivity::class.java)
+
+        startActivity(intent)
     }
-     */
+
+
 }
+
