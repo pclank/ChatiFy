68295be9e128b107780aa1924f16789404d387c6
pclank <31415965+pclank@users.noreply.github.com>
2020-05-24 01:33:27 +0300

    Added User Class and Relative Functionality
    Signed-off-by: pclank <31415965+pclank@users.noreply.github.com>

---
+++ app/src/main/java/com/example/chatify/User.kt
@@ -0,0 +1,43 @@
+package com.example.chatify
+
+class User
+{
+    var username: String? = ""
+    var spotify_user: String? = null
+    var premium_priv: Boolean = false
+    val user_id: Int = (0..9999).random()               // TODO (Presentation Only!)
+    var warnings: Int = 0
+    var ban: Boolean = false
+
+    fun enablePremiumFeatures(): Boolean
+    {
+        // TODO (Add Further Functionality)
+        this.premium_priv = true
+
+        return true                                     // Confirm Success
+    }
+
+    fun addSpotifyData(username: String)
+    {
+        this.spotify_user = username
+    }
+
+    fun userDeletion()                                  // From DeletionConfirmationScreen
+    {
+        // TODO (Remove From Database)
+        this.username = null
+        this.spotify_user = null
+        this.premium_priv = false
+        this.ban = true
+    }
+
+    fun addWarning()
+    {
+        this.warnings++
+    }
+
+    fun addBan()
+    {
+        this.ban = true
+    }
+}
