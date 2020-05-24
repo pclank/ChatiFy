6151dd79290994e09eee6d7d290e45ac9374753c
pclank <31415965+pclank@users.noreply.github.com>
2020-05-25 02:48:54 +0300

    Removed Some DateTime Related Functionality, Based on Crashes On Older Devices.
    Signed-off-by: pclank <31415965+pclank@users.noreply.github.com>

--- app/src/main/java/com/example/chatify/Playlist.kt
+++ app/src/main/java/com/example/chatify/Playlist.kt
@@ -1,14 +1,10 @@
 package com.example.chatify
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import java.time.LocalDateTime.now
-
 class Playlist (title: String, private val spotify_user: String)
 {
     var title = title
     var p_list: Array<Song>? = null
-    @RequiresApi(Build.VERSION_CODES.O)
-    val timestamp: String = now().toString()
+
+    val timestamp: String = ""
 
 }
--- app/src/main/java/com/example/chatify/ui/myfavouriteartists/MyFavouriteArtists.kt
+++ app/src/main/java/com/example/chatify/ui/myfavouriteartists/MyFavouriteArtists.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
