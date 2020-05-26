d8a72a911c39377930a6f657ca7aa9a56cb44a6b
nikos <soadnick@outlook.com>
2020-05-26 18:01:26 +0300

    initial account deletion screen

---
+++ app/src/main/java/com/example/chatify/AccountDelete.kt
@@ -0,0 +1,13 @@
+package com.example.chatify
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountDelete : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.account_delete)
+    }
+
+
+}
