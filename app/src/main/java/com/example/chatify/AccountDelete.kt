58c5556214e761f7a9345b5579e9cdfe99a1912e
nikos <soadnick@outlook.com>
2020-05-26 18:28:48 +0300

    final account deletion screen - missing some functionality

--- app/src/main/java/com/example/chatify/AccountDelete.kt
+++ app/src/main/java/com/example/chatify/AccountDelete.kt
@@ -1,7 +1,10 @@
 package com.example.chatify
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.account_delete.*
 
 class AccountDelete : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,4 +13,27 @@ class AccountDelete : AppCompatActivity() {
     }
 
 
+
+    fun deleteAccount(view: View) {
+        val password = editText2.text.toString()
+
+        val builder = AlertDialog.Builder(this)
+        builder.setPositiveButton("OK", null)
+
+        if (password.isNotEmpty()) {
+            // TODO collect user data
+            // TODO delete user from DB
+            // TODO delete user data
+            builder.setTitle("Deletion Successful")
+            builder.setMessage("Your account and user data have been deleted. " +
+                    "You are now disconnected.")
+        }
+        else {
+            builder.setTitle("Empty password")
+            builder.setMessage("Please fill in your ChatiFy password " +
+                    "in order to verify the account deletion.")
+        }
+
+        builder.show()
+    }
 }
