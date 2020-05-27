7016728137db42cb5fb2b684cdd9db227216386e
nikos <soadnick@outlook.com>
2020-05-27 14:21:09 +0300

    added option highlighting on recyclerview

--- app/src/main/java/com/example/chatify/RecyclerAdapter.kt
+++ app/src/main/java/com/example/chatify/RecyclerAdapter.kt
@@ -1,15 +1,17 @@
 package com.example.chatify
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recyclerview_row.view.*
 
 
 class RecyclerAdapter(private val context: ReportPage, private val reasonsList: ArrayList<String>) :
     RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private var selectedPosition = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.recyclerview_row, parent, false))
@@ -21,8 +23,16 @@ class RecyclerAdapter(private val context: ReportPage, private val reasonsList: 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.reasonName?.text = reasonsList[position]
+
+        holder.itemView.setBackgroundColor(Color.parseColor("#5bb7ed"))
+        if (selectedPosition == position)
+            holder.itemView.setBackgroundColor(Color.parseColor("#5bb7ed"))
+        else
+            holder.itemView.setBackgroundColor(Color.parseColor("#fafafa"))
+
         holder.itemView.setOnClickListener {
-            Toast.makeText(context, reasonsList[position], Toast.LENGTH_LONG).show()
+            selectedPosition = position
+            notifyDataSetChanged()
         }
     }
 
