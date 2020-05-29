a0ee03ad752bf19e144a0ec099f7d5bdfb64b03a
nikos <soadnick@outlook.com>
2020-05-29 18:00:35 +0300

    fix report screen not appearing when flagging a message

--- app/src/main/java/com/example/chatify/MainActivity.kt
+++ app/src/main/java/com/example/chatify/MainActivity.kt
@@ -248,6 +248,8 @@ class MainActivity : AppCompatActivity() {
         // TODO Add Flagging Functionality
         // TODO Change Message to Removed Form
 
+        val intent = Intent(this, ReportPage::class.java)
+        startActivity(intent)
 
         return true
     }
